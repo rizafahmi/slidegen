26daca27aee9132335312ecc33fc0d6809fc4a6e
Riza Fahmi <rizafahmi@gmail.com>
2025-07-04 07:17:24 +0700

    Update db CI to add password

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :slidegen, Slidegen.Repo,
   username: "riza",
-  password: "",
+  password: "secret",
   hostname: "localhost",
   database: "slidegen_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
